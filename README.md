8ce1b85f824f53f593f2dade13d6181f0e79d979
dolly <timohakt@yandex.ru>
2018-03-05 09:39:08 +0300

    Update bars.py and README Add bars.json

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+**bars.py** расчитывает:
+ - самый большой бар по количеству мест
+ - самый маленький бар по количеству мест
+ - самый близкий бар (текущие gps координаты пользователь вводит с клавиатуры )
 
 # Как запустить
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5 и библиотеки **geopy**
+
+```bash
+
+pip install geopy
+
+```
 
 Запуск на Linux:
 
 ```bash
 
 $ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+Enter your longtude: 37.6
+Enter your latutude: 55.7
+The biggest bar: Спорт бар «Красная машина» with 450 seats
+The smallest bar: БАР. СОКИ with 0 seats
+The closest bar: БИР-ТАЙМ distance: 1893.25 meters
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,21 +1,55 @@
 import json
+import os
+from geopy.distance import vincenty as distance
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as infile:
+        json_content = json.loads(infile.read())
+    return json_content
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(json_content):
+    biggest_bar = max(json_content['features'],
+                      key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return biggest_bar
+
+def get_smallest_bar(json_content):
+    smallest_bar = min(json_content['features'],
+                       key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return smallest_bar
 
 
-def get_smallest_bar(data):
-    pass
-
-
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(json_content, longitude, latitude):
+    closest_bar = min(json_content['features'],
+                      key=lambda bar: distance(bar['geometry']['coordinates'], (longitude, latitude)).km)
+    return closest_bar
 
 
 if __name__ == '__main__':
-    pass
+    input_file = 'bars.json'
+    if not os.path.exists(input_file):
+        exit("File {} doesn't exists")
+    json_content = load_data('bars.json')
+    if json_content:
+        try:
+            my_longtude = float(input('Enter your longtude: '))
+            my_latitude = float(input('Enter your latutude: '))
+        except ValueError:
+            exit("Longtude and latitude should be a numbers")
+        biggest_bar = get_biggest_bar(json_content)
+        smallest_bar = get_smallest_bar(json_content)
+        closest_bar = get_closest_bar(json_content, my_longtude, my_latitude)
+
+        name_biggest_bar = biggest_bar['properties']['Attributes']['Name']
+        seats_count_biggest_bar = biggest_bar['properties']['Attributes']['SeatsCount']
+
+        name_smallest_bar = smallest_bar['properties']['Attributes']['Name']
+        seats_count_smallest_bar = smallest_bar['properties']['Attributes']['SeatsCount']
+
+        name_closest_bar = closest_bar['properties']['Attributes']['Name']
+        distance_closet_bar = distance(closest_bar['geometry']['coordinates'], (my_longtude, my_latitude)).meters
+
+        print(u'The biggest bar: {} with {} seats'.format(name_biggest_bar, seats_count_biggest_bar))
+        print(u'The smallest bar: {} with {} seats'.format(name_smallest_bar, seats_count_smallest_bar))
+        print(u'The closest bar: {} distance: {} meters'.format(name_closest_bar, round(distance_closet_bar,2)))
