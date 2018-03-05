2f6efe6e56ef8e4ffe585919018c89025f5520c9
dolly <timohakt@yandex.ru>
2018-03-05 09:56:27 +0300

    Fix pep8 warnings in bars.py

--- bars.py
+++ bars.py
@@ -10,20 +10,24 @@ def load_data(filepath):
 
 
 def get_biggest_bar(json_content):
-    biggest_bar = max(json_content['features'],
-                      key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return biggest_bar
+    bg_bar = \
+        max(json_content['features'],
+            key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return bg_bar
 
 def get_smallest_bar(json_content):
-    smallest_bar = min(json_content['features'],
-                       key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return smallest_bar
+    sm_bar = \
+        min(json_content['features'],
+            key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return sm_bar
 
 
 def get_closest_bar(json_content, longitude, latitude):
-    closest_bar = min(json_content['features'],
-                      key=lambda bar: distance(bar['geometry']['coordinates'], (longitude, latitude)).km)
-    return closest_bar
+    cl_bar = \
+        min(json_content['features'],
+            key=lambda bar: distance(bar['geometry']['coordinates'],
+                                     (longitude, latitude)).km)
+    return cl_bar
 
 
 if __name__ == '__main__':
@@ -37,19 +41,28 @@ if __name__ == '__main__':
             my_latitude = float(input('Enter your latutude: '))
         except ValueError:
             exit("Longtude and latitude should be a numbers")
-        biggest_bar = get_biggest_bar(json_content)
-        smallest_bar = get_smallest_bar(json_content)
-        closest_bar = get_closest_bar(json_content, my_longtude, my_latitude)
+    biggest_bar = get_biggest_bar(json_content)
+    smallest_bar = get_smallest_bar(json_content)
+    closest_bar = get_closest_bar(json_content, my_longtude, my_latitude)
 
-        name_biggest_bar = biggest_bar['properties']['Attributes']['Name']
-        seats_count_biggest_bar = biggest_bar['properties']['Attributes']['SeatsCount']
+    name_biggest_bar = \
+        biggest_bar['properties']['Attributes']['Name']
+    seats_count_biggest_bar = \
+        biggest_bar['properties']['Attributes']['SeatsCount']
 
-        name_smallest_bar = smallest_bar['properties']['Attributes']['Name']
-        seats_count_smallest_bar = smallest_bar['properties']['Attributes']['SeatsCount']
+    name_smallest_bar = \
+        smallest_bar['properties']['Attributes']['Name']
+    seats_count_smallest_bar = \
+        smallest_bar['properties']['Attributes']['SeatsCount']
 
-        name_closest_bar = closest_bar['properties']['Attributes']['Name']
-        distance_closet_bar = distance(closest_bar['geometry']['coordinates'], (my_longtude, my_latitude)).meters
+    name_closest_bar = \
+        closest_bar['properties']['Attributes']['Name']
+    distance_closet_bar = distance(closest_bar['geometry']['coordinates'],
+                                   (my_longtude, my_latitude)).meters
 
-        print(u'The biggest bar: {} with {} seats'.format(name_biggest_bar, seats_count_biggest_bar))
-        print(u'The smallest bar: {} with {} seats'.format(name_smallest_bar, seats_count_smallest_bar))
-        print(u'The closest bar: {} distance: {} meters'.format(name_closest_bar, round(distance_closet_bar,2)))
+    print(u'The biggest bar: {} with {} seats'
+          .format(name_biggest_bar, seats_count_biggest_bar))
+    print(u'The smallest bar: {} with {} seats'
+          .format(name_smallest_bar, seats_count_smallest_bar))
+    print(u'The closest bar: {} distance: {} meters'
+          .format(name_closest_bar, round(distance_closet_bar, 2)))
